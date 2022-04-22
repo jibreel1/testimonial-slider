9750db9e99e08d7927c2247ab2884057b64e3c28
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2022-04-22 22:43:52 +0100

    edited carousel for desktop

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const carouselTrack = document.querySelector(".carousel-track");
 const slides = Array.from(carouselTrack.children);
 const mobilePrevBtn = document.querySelector(".btn1");
 const mobileNextBtn = document.querySelector(".btn2");
+const desktopPrevBtn = document.querySelector(".btn3");
+const desktopNextBtn = document.querySelector(".btn4");
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
@@ -12,10 +14,16 @@ const setSlidePosition = (slide, index) => {
 slides.forEach(setSlidePosition);
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-   track.style.transform = "translateX(-" + targetSlide.style.left + ")";
-   currentSlide.classList.remove("current-slide");
-   targetSlide.classList.add("current-slide");
+   if (targetSlide) {
+      track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+      currentSlide.classList.remove("current-slide");
+      targetSlide.classList.add("current-slide");
+   } else {
+      return;
+   }
 };
+
+//carousel for mobile
 
 //when left arrow is clicked, move left
 mobilePrevBtn.addEventListener("click", () => {
@@ -27,6 +35,24 @@ mobilePrevBtn.addEventListener("click", () => {
 
 //when right arrow is clicked, move right
 mobileNextBtn.addEventListener("click", () => {
+   const currentSlide = carouselTrack.querySelector(".current-slide");
+   const nextSlide = currentSlide.nextElementSibling;
+
+   moveToSlide(carouselTrack, currentSlide, nextSlide);
+});
+
+//carousel for desktop
+
+//when left arrow is clicked, move left
+desktopPrevBtn.addEventListener("click", () => {
+   const currentSlide = carouselTrack.querySelector(".current-slide");
+   const prevSlide = currentSlide.previousElementSibling;
+
+   moveToSlide(carouselTrack, currentSlide, prevSlide);
+});
+
+//when right arrow is clicked, move right
+desktopNextBtn.addEventListener("click", () => {
    const currentSlide = carouselTrack.querySelector(".current-slide");
    const nextSlide = currentSlide.nextElementSibling;
 
